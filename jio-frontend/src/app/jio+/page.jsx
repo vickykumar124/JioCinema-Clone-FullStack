f13b408c2e62b88a3e8d10466bfb2e7b38f19a77
vicky kumar <computerscience027@gmail.com>
2026-06-20 16:00:37 +0530

    fix jio plus deploy

--- jio-frontend/src/app/jio+/page.jsx
+++ jio-frontend/src/app/jio+/page.jsx
@@ -1,48 +1,47 @@
-import { api, ENDPOINT, getStreamingVideoThumbnail } from "@/lib/api";
+export const dynamic = "force-dynamic";
+
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
 import { PlayCircleIcon } from "lucide-react";
 
 export default async function JioPlusPage() {
-  let videos = [];
-
-  try {
-    const res = await api.get(ENDPOINT.fetchAllStreamingVideos);
-    videos = res?.data?.data || [];
-  } catch (error) {
-    console.log("Video Fetch Error:", error);
-  }
+  const videos = [];
 
   return (
-    <main className="h-screen mt-20 p-8">
+    <main className="min-h-screen mt-20 p-8">
       <h1 className="text-2xl font-medium mb-6">
         Jio+ Premium Videos
       </h1>
 
-      <ul
-        className={cn(
-          "flex gap-4 w-full overflow-scroll scrollbar-hide p-4"
-        )}
-      >
-        {videos?.map((video, index) => (
-          <Link
-            key={index}
-            href={`/jio+/watch?id=${video.id}`}
-            className="relative flex items-center justify-center"
-          >
-            <Image
-              src={getStreamingVideoThumbnail(video.id)}
-              alt={video.title || "video"}
-              width={200}
-              height={300}
-              className="min-w-[200px] h-[300px] rounded-lg object-cover"
-              quality={30}
-            />
-            <PlayCircleIcon className="absolute" />
-          </Link>
-        ))}
-      </ul>
+      {videos.length === 0 ? (
+        <div className="text-gray-400">
+          No Premium Videos Available
+        </div>
+      ) : (
+        <ul
+          className={cn(
+            "flex gap-4 w-full overflow-scroll scrollbar-hide p-4"
+          )}
+        >
+          {videos.map((video, index) => (
+            <Link
+              key={index}
+              href={`/jio+/watch?id=${video.id}`}
+              className="relative flex items-center justify-center"
+            >
+              <Image
+                src={"/placeholder.jpg"}
+                alt={video.title || "video"}
+                width={200}
+                height={300}
+                className="min-w-[200px] h-[300px] rounded-lg object-cover"
+              />
+              <PlayCircleIcon className="absolute" />
+            </Link>
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
